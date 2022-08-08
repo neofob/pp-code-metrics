71bfd4bb945963ade529e928582b3e06a0510822
Tuan T. Pham <tuan@vt.edu>
2022-08-08 00:30:19 -0400

    Initial ingestion to influxdb
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- get_metrics.py
+++ get_metrics.py
@@ -6,6 +6,10 @@ import pprint
 
 import requests
 
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
 """
 Example:
 CONFIG_FILE=my_settings.yml ./get_metrics.py
@@ -19,6 +23,16 @@ my_config = {}
 with open(config_file, "r") as file:
     my_config = yaml.safe_load(file)
 
+
+# InfluxDB
+influxdb_bucket = my_config['InfluxDB']['Bucket']
+influxdb_api_token = my_config['InfluxDB']['API_TOKEN']
+influxdb_host = my_config['InfluxDB']['Host']
+influxdb_port = my_config['InfluxDB']['Port']
+influxdb_url = 'http://' + influxdb_host + ':' + str(influxdb_port)
+influxdb_org = my_config['InfluxDB']['Org']
+client = InfluxDBClient(url=influxdb_url, token=influxdb_api_token, org=influxdb_org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 """
 TODO: iterate through "Nodes"
@@ -35,9 +49,12 @@ for node in my_config['Nodes'].keys():
     node_port = my_config['Nodes'][node]['Port']
     node_key = my_config['Nodes'][node]['Key']
     node_metric_field = my_config['Nodes'][node]['Metric']
-    uri = 'http://' + node_host + ':' + str(node_port) + '/' + node_key + '&Stats/json'
-    r = requests.get(uri)
+    node_uri = 'http://' + node_host + ':' + str(node_port) + '/' + node_key + '&Stats/json'
+    node_r = requests.get(node_uri)
     node_metrics = {}
-    for metric in r.json()[node_metric_field].keys():
-        node_metrics[metric] = r.json()[node_metric_field][metric][:-1]
+    for metric in node_r.json()[node_metric_field].keys():
+        node_metrics[metric] = float(node_r.json()[node_metric_field][metric][:-1])
     pp.pprint(node_metrics)
+    # test only Temp2 for now
+    p = Point("NodeSensor").tag("location", "Soil").field("temperature", node_metrics['Temp2'])
+    write_api.write(bucket=influxdb_bucket, record=p)
