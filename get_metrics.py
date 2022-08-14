c7da82e61eb378885d2c2b17b457dd7211c0406e
Tuan T. Pham <tuan@vt.edu>
2022-08-14 14:51:20 -0400

    Add error checking when we poll metrics
    get_metrics.py:
        * We do not ingest data if we fail to query metrics from
        the node. It could be that:
        1. The response is not 200 OK
        2. The metric field is an empty string
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- get_metrics.py
+++ get_metrics.py
@@ -3,6 +3,7 @@
 import os
 import yaml
 import pprint
+import time
 
 import requests
 
@@ -38,32 +39,48 @@ for node_k, node in my_config['Nodes'].items():
     #if "Garden" != node_k:
     #    continue
 
-    node_host = node['Host']
-    node_port = node['Port']
-    node_key = node['Key']
-    node_metric_field = node['Metric']
-    node_uri = 'http://' + node_host + ':' + str(node_port) + '/' + node_key + '&Stats/json'
-    node_r = requests.get(node_uri)
-    node_metrics = {}
-    node_tags = node['Tags']
-    node_fields = node['Fields']
-    node_measurement = node['Measurement']
-    point_fields = {}
-    for metric, c_field in node_fields.items():
-        if c_field['chomp']:
-            node_metrics[metric] = float(node_r.json()[node_metric_field][metric][:-1])
-        else:
-            node_metrics[metric] = float(node_r.json()[node_metric_field][metric])
-
-        point_fields[metric] = c_field['field']
-        #pp.pprint(point_fields[metric])
-        p = Point(node_measurement).field(point_fields[metric], node_metrics[metric])
-        # Tags for each node
-        for k,v in node_tags.items():
-            p.tag(k,v)
-        # Extra tags for each field
-        if 'Tags' in c_field:
-            for k,v in c_field['Tags'].items():
+    for n_try in range(3):
+        try_again = False
+        node_host = node['Host']
+        node_port = node['Port']
+        node_key = node['Key']
+        node_metric_field = node['Metric']
+        node_uri = 'http://' + node_host + ':' + str(node_port) + '/' + node_key + '&Stats/json'
+        node_r = requests.get(node_uri)
+        if 200 != node_r.status_code:
+            time.sleep(5)
+            continue
+        node_metrics = {}
+        node_tags = node['Tags']
+        node_fields = node['Fields']
+        node_measurement = node['Measurement']
+        point_fields = {}
+        for metric, c_field in node_fields.items():
+            try:
+                if c_field['chomp']:
+                    node_metrics[metric] = float(node_r.json()[node_metric_field][metric][:-1])
+                else:
+                    node_metrics[metric] = float(node_r.json()[node_metric_field][metric])
+            except ValueError:
+                try_gain = True
+                continue
+            point_fields[metric] = c_field['field']
+            #pp.pprint(point_fields[metric])
+            p = Point(node_measurement).field(point_fields[metric], node_metrics[metric])
+            # Tags for each node
+            for k,v in node_tags.items():
                 p.tag(k,v)
-        write_api.write(bucket=influxdb_bucket, record=p)
-    pp.pprint(node_metrics)
+            # Extra tags for each field
+            if 'Tags' in c_field:
+                for k,v in c_field['Tags'].items():
+                    p.tag(k,v)
+            write_api.write(bucket=influxdb_bucket, record=p)
+
+        if try_again:
+            time.sleep(5)
+            continue
+        else:
+            pp.pprint(node_metrics)
+            break
+
+client.close()
